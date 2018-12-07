920e02309353b64208d38e81285f131284b3fde0
Tadeu Santos <tadeu.santos@accenture.com>
2018-12-07 11:36:05 -0300

    Unit test

--- helloworld/src/test/java/com/devops/helloworld/HelloworldApplicationTests.java
+++ helloworld/src/test/java/com/devops/helloworld/HelloworldApplicationTests.java
@@ -1,16 +1,30 @@
 package com.devops.helloworld;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HelloworldApplicationTests {
 
+	@Autowired
+	private MockMvc mock;
+	
 	@Test
-	public void contextLoads() {
+	public void correctHelloWorld() throws Exception {
+		mock.perform(get("/world/hello"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.response").value("Hello World Devops"));
 	}
 
 }
